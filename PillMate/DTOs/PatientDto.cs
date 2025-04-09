9c0a2c5a3cd022947876b1742261505a043d9aed
YHW <guddn505@naver.com>
2025-04-09 22:09:28 +0900

    Dto 코드 추가

--- PillMate/DTOs/PatientDto.cs
+++ PillMate/DTOs/PatientDto.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace PillMate.DTOs
 {
-    internal class PatientDto
+    public class PatientDto
     {
+        public int Id { get; set; }
+        public string Hwanja_Name { get; set; }
+        public string Hwanja_Gender { get; set; }
+        public string Hwanja_No { get; set; }
+        public string Hwanja_Room { get; set; }
+        public string Hwanja_PhoneNumber { get; set; }
+        public string Bohoja_Name { get; set; }
+        public string Bohoja_PhoneNumber { get; set; }
+    }
+
+    public class CreatePatientDto
+    {
+        public string Hwanja_Name { get; set; }
+        public string Hwanja_Gender { get; set; }
+        public string Hwanja_No { get; set; }
+        public string Hwanja_Room { get; set; }
+        public string Hwanja_PhoneNumber { get; set; }
+        public string Bohoja_Name { get; set; }
+        public string Bohoja_PhoneNumber { get; set; }
+    }
+
+    public class UpdatePatientDto : CreatePatientDto
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeletePatientDto
+    {
+        public int Id { get; set; }
     }
 }
--- PillMate/DTOs/PillDto.cs
+++ PillMate/DTOs/PillDto.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PillMate.DTOs
+namespace PillMate.DTO
 {
-    internal class PillDto
+    public class PillDto
     {
+        public int Id { get; set; }
+        public string Yank_Name { get; set; }
+        public int Yank_Cnt { get; set; }
+        public string Yank_Num { get; set; }
+    }
+
+    public class CreatePillDto
+    {
+        public string Yank_Name { get; set; }
+        public int Yank_Cnt { get; set; }
+        public string Yank_Num { get; set; }
+    }
+
+    public class UpdatePillDto : CreatePillDto
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeletePillDto
+    {
+        public int Id { get; set; }
     }
 }
+
