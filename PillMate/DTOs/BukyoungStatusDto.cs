f5af42d380341ce73ee90ee0f1641414254fe237
YHW <guddn505@naver.com>
2025-04-09 22:11:05 +0900

    Revert "Dto 코드 추가"
    This reverts commit 9c0a2c5a3cd022947876b1742261505a043d9aed.

--- PillMate/DTOs/BukyoungStatusDto.cs
+++ PillMate/DTOs/BukyoungStatusDto.cs
@@ -4,33 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PillMate.DTO
+namespace PillMate.DTOs
 {
-    public class BukyoungStatusDto
+    internal class BukyoungStatusDto
     {
-        public int Id { get; set; }
-        public string Hwanja_No { get; set; }
-        public string Hwanja_Name { get; set; }
-        public bool Bukyoung_Chk { get; set; }
-        public int PatientId { get; set; }
-    }
-
-    public class CreateBukyoungStatusDto
-    {
-        public string Hwanja_No { get; set; }
-        public string Hwanja_Name { get; set; }
-        public bool Bukyoung_Chk { get; set; }
-        public int PatientId { get; set; }
-    }
-
-    public class UpdateBukyoungStatusDto : CreateBukyoungStatusDto
-    {
-        public int Id { get; set; }
-    }
-
-    public class DeleteBukyoungStatusDto
-    {
-        public int Id { get; set; }
     }
 }
-
--- PillMate/DTOs/PillDto.cs
+++ PillMate/DTOs/PillDto.cs
@@ -4,31 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PillMate.DTO
+namespace PillMate.DTOs
 {
-    public class PillDto
+    internal class PillDto
     {
-        public int Id { get; set; }
-        public string Yank_Name { get; set; }
-        public int Yank_Cnt { get; set; }
-        public string Yank_Num { get; set; }
-    }
-
-    public class CreatePillDto
-    {
-        public string Yank_Name { get; set; }
-        public int Yank_Cnt { get; set; }
-        public string Yank_Num { get; set; }
-    }
-
-    public class UpdatePillDto : CreatePillDto
-    {
-        public int Id { get; set; }
-    }
-
-    public class DeletePillDto
-    {
-        public int Id { get; set; }
     }
 }
-
