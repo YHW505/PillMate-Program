2d7669cb87b6abd9594edc9f01342203ed7e1a22
YHW <guddn505@naver.com>
2025-04-09 22:17:15 +0900

    add Dto Code

--- PillMate/DTOs/PatientDto.cs
+++ PillMate/DTOs/PatientDto.cs
@@ -4,9 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PillMate.DTOs
+namespace PillMate.DTO
 {
-    internal class PatientDto
+    public class PatientDto
     {
+        public int Id { get; set; }
+        public string Hwanja_Name { get; set; }
+        public string Hwanja_Gender { get; set; }
+        public string Hwanja_No { get; set; }
+        public string Hwanja_Room { get; set; }
+        public string Hwanja_PhoneNumber { get; set; }
+        public string Bohoja_Name { get; set; }
+        public string Bohoja_PhoneNumber { get; set; }
+    }
+
+    public class CreatePatientDto
+    {
+        public string Hwanja_Name { get; set; }
+        public string Hwanja_Gender { get; set; }
+        public string Hwanja_No { get; set; }
+        public string Hwanja_Room { get; set; }
+        public string Hwanja_PhoneNumber { get; set; }
+        public string Bohoja_Name { get; set; }
+        public string Bohoja_PhoneNumber { get; set; }
+    }
+
+    public class UpdatePatientDto : CreatePatientDto
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeletePatientDto
+    {
+        public int Id { get; set; }
     }
 }
+
