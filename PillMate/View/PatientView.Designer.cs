b66cec5101c4d4ec38901824b1712969d6866bf3
YHW <guddn505@naver.com>
2025-04-14 21:13:21 +0900

    PillPage의 수정버튼 삭제, textbox visible 옵션 삭제, 새로고침 버튼 삭제 / 메인화면에서 패널 추가, 각 패널로 이동할 수 있는 버튼 추가

--- PillMate/View/PatientView.Designer.cs
+++ PillMate/View/PatientView.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // PatientView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form3";
+            this.Name = "PatientView";
+            this.Text = "PatientList";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
