0b2d3cef0aac577f82f775f3c4481a46493d52e2
YHW <guddn505@naver.com>
2025-05-07 23:04:04 +0900

    복용하는 약 리스트 추가, 홈화면 그리드뷰 추가, UI위치 조정

---
+++ PillMate/DTOs/TakenMedicineDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PillMate.DTO
+{
+    public class TakenMedicineDto
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int PillId { get; set; }
+        public string Dosage { get; set; }
+        public PillDto Pill { get; set; }
+        public PatientDto Patient { get; set; }
+    }
+
+    public class CreateTakenMedicineDto
+    {
+        public int PatientId { get; set; }
+        public int PillId { get; set; }
+        public string Dosage { get; set; }
+        public PillDto Pill { get; set; }
+        public PatientDto Patient { get; set; }
+    }
+
+    public class UpdateTakenMedicineDto : CreateTakenMedicineDto
+    {
+        public int Id { get; set; }
+    }
+
+    public class DeleteTakenMedicineDto
+    {
+        public int Id { get; set; }
+    }
+}
--- PillMate/View/BukyoungView.Designer.cs
+++ PillMate/View/BukyoungView.Designer.cs
@@ -28,31 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // dataGridView1
             // 
-            this.button1.Location = new System.Drawing.Point(274, 137);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(222, 104);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "BukyoungView";
-            this.button1.UseVisualStyleBackColor = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(591, 324);
+            this.dataGridView1.TabIndex = 0;
             // 
             // BukyoungView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Name = "BukyoungView";
             this.AutoSize = true;
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "BukyoungView";
+            this.Size = new System.Drawing.Size(798, 420);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
